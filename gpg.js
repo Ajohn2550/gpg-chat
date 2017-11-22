1e8324befb0a8d524ef363f937fd05bf68d344fc
Alex Johnson <alex@johnsonwebware.com>
2017-11-22 00:25:57 -0700

    Second tests

--- gpg.js
+++ gpg.js
@@ -1,10 +1,19 @@
 const gpg = require('./lib/gpg');
 
-gpg.encrypt('This is an elaborate test', ['-r alex@johnsonwebware.com'], (err, encrypted) => {
+gpg.encryptAndSign('This is an elaborate test', ['-r alex@johnsonwebware.com'], (err, encrypted) => {
     if (err) console.error(err);
     console.log(encrypted);
     gpg.decrypt(encrypted, [], (err2, decrypted) => {
         if (err2) console.error(err2);
         console.log(decrypted.toString());
+    });
+});
+
+gpg.clearsign('This is not an elaborate test', ['-r alex@johnsonwebware.com'], (err, signed) => {
+    if(err) console.error(err);
+    console.log(signed);
+    gpg.verifySignature(signed, [], (err2, result) => {
+        if (err2) console.error(err2);
+        console.log(result.toString());
     });
 });
